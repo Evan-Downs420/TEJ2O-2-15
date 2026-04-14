a9fee5ea5dc7b9b5beda0e956067ac140ab35cf6
Evan-Downs420 <evan.downs@stu.ocsb.ca>
2026-04-14 12:47:22 -0400

    added code

--- main.ts
+++ main.ts
@@ -1,8 +1,34 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Evan
+ * Created on: Apr
+ * This program will go in a full loop 
 */
 
-basic.showString('Hello, World!')
+// variables
+let sprite: game.LedSprite = null
+let loopCounter1 = 0
+let loopCounter2 = 0
+
+// setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+//  press a
+input.onButtonPressed(Button.A, function () {
+    // setup
+    basic.clearScreen()
+    sprite = game.createSprite(0, 0)
+    loopCounter1 = 0
+    while (loopCounter1 < 4) {
+        loopCounter1 = loopCounter1 + 1
+        loopCounter2 = 0
+        while (loopCounter2 < 4) {
+            sprite.move(1)
+            basic.pause(200)
+            loopCounter2 = loopCounter2 + 1
+        }
+        sprite.turn(Direction.Right, 90)
+    }
+    sprite.delete()
+})
